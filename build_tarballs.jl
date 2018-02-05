4567b85f73672e024ef1cb13b14ec8139c8c38cc
Avik Sengupta <avik@sengupta.net>
2018-02-05 12:56:58 +0000

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -1,28 +1,45 @@
 using BinaryBuilder
 
+# These are the platforms built inside the wizard
 platforms = [
-BinaryProvider.Linux(:x86_64, :glibc),
-BinaryProvider.MacOS(),
-BinaryProvider.Windows(:x86_64)
+    BinaryProvider.Linux(:i686, :glibc),
+  BinaryProvider.Linux(:x86_64, :glibc),
+  BinaryProvider.Linux(:aarch64, :glibc),
+  BinaryProvider.Linux(:armv7l, :glibc),
+  BinaryProvider.Linux(:powerpc64le, :glibc),
+  BinaryProvider.MacOS(),
+  BinaryProvider.Windows(:i686),
+  BinaryProvider.Windows(:x86_64)
 ]
 
+
+# If the user passed in a platform (or a few, comma-separated) on the
+# command-line, use that instead of our default platforms
+if length(ARGS) > 0
+    platforms = platform_key.(split(ARGS[1], ","))
+end
+info("Building for $(join(triplet.(platforms), ", "))")
+
+# Collection of sources required to build LibStemmer
 sources = [
-"6530c3638f29f5b03bc3786bb9212e39adaf3bf15b5b0a483b2eb5111f7f416e" =>
-"http://snowball.tartarus.org/dist/libstemmer_c.tgz",
+    "https://github.com/zvelo/libstemmer.git" =>
+    "78c149a3a6f262a35c7f7351d3f77b725fc646cf",
 ]
 
 script = raw"""
-cd libstemmer_c/
-export CPPFLAGS="-fPIC"
+cd $WORKSPACE/srcdir
+cd libstemmer/
+sed -i -e 's/ADD_LIBRARY(stemmer/ADD_LIBRARY(stemmer SHARED/' CMakeLists.txt
+cmake -DCMAKE_INSTALL_PREFIX=/ -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain
 make
-$CC -shared -o libstemmer.so libstemmer.o
-cp libstemmer.so $DESTDIR/
-exit
+make install
 
 """
 
 products = prefix -> [
-	LibraryProduct(prefix,"libstemmer")
+    LibraryProduct(prefix,"libstemmer")
 ]
 
-autobuild(pwd(), "", platforms, sources, script, products)
+
+# Build the given platforms using the given sources
+hashes = autobuild(pwd(), "LibStemmer", platforms, sources, script, products)
